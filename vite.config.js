a84d2636ecc7a25f60ea74fd0f7f466804aa316d
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2023-02-17 16:46:02 -0500

    ,

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  build: {
+    chunkSizeWarningLimit: 250 // ajusta el límite de tamaño de chunk a 1000 kBs
+  }
 })
